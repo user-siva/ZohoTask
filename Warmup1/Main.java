ec07b1e33d1fdbf721e994757d45409d07308d4d
user-siva <sivag0503@gmail.com>
2024-06-28 12:53:27 +0530

    init

---
+++ Warmup1/Main.java
@@ -0,0 +1,7 @@
+package Warmup1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Warmup1/SleepIn.java
@@ -0,0 +1,7 @@
+package Warmup1;
+
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
